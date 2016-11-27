e9b8e83c53224885c15b5dbc2579f1b786c1d563
SeungyeonKang <rillyppoppy@naver.com>
2016-11-27 11:11:19 +0900

    add comment to export.cs (English)

--- Exporter.cs
+++ Exporter.cs
@@ -18,10 +18,10 @@ namespace Steganography
         }
 
         /// <summary>
-        /// Checks selected file before starting the process.
+        /// Checks selected file before starting the process(Export 전에 선택된 파일 체크)
         /// </summary>
         /// <returns></returns>
-        private int CheckBeforeExport() //Export 전에 선택된 파일 체크
+        private int CheckBeforeExport() 
         {
             try
             {
@@ -29,7 +29,7 @@ namespace Steganography
             }
             catch
             {
-                return 0; // 예외 발생시 0반환
+                return 0; // 예외 발생시 0반환 (if error catched, retuen 0)
             }
         }
 
@@ -48,21 +48,21 @@ namespace Steganography
         }
 
         /// <summary>
-        /// Exports the text length using the bottom pixels line of the selected image file.
+        /// Exports the text length using the bottom pixels line of the selected image file (선택된 이미지 파일의 가장 마지막 줄 픽셀에서 텍스트 길이 추출). 
         /// </summary>
-        /// <returns>Returns the length of the text.</returns>
+        /// <returns>Returns the length of the text(텍스트의 길이 반환).</returns>
         private int ExportTextLength()
         {
             var bitmap = new Bitmap(_form.ExportPictureBoxImage);
             string thirtyBytes = string.Empty, textLength = string.Empty;
 
-            for (int i = 0; i < 10; i++)  // gettint last 30 bytes of the image
+            for (int i = 0; i < 10; i++)  // gettint last 30 bytes of the image (이미지의 마지막 줄 바로 위의 줄 30바이트 얻음)
             {
                 thirtyBytes += Convert.ToString(bitmap.GetPixel(i, _form.ImageHeight - 1).ToArgb(), 2).Substring(8);
-            }//이미지의 마지막 줄 바로 위의 줄 30바이트 얻음
+            }
 
             int pointer = 7;
-            for (int i = 0; i < 30; i++)   // getting the last bit of the each bytes
+            for (int i = 0; i < 30; i++)   // getting the last bit of the each bytes (각 바이트의 마지막 비트 얻음)
             {
                 textLength += thirtyBytes.Substring(pointer, 1);
                 //각 바이트의 7번째 자리 값(즉 LSB비트)를 뽑아서 저장된 메시지의 길이 구함->2진수
@@ -89,7 +89,7 @@ namespace Steganography
         }
 
         /// <summary>
-        /// Exports the text from the selected image file by starting from top left corner.
+        /// Exports the text from the selected image file by starting from top left corner. (이미지 첫줄의 왼쪽 픽셀부터 이미지 추출)
         /// </summary>
         public void Export()
         {
@@ -117,11 +117,11 @@ namespace Steganography
                 string importedText = null;
 
 
-                for (int l = 0; l < totalBytes; l++) // Getting the last bit of each bytes and stores to 'bytesToExportast'
+                for (int l = 0; l < totalBytes; l++) // Getting the last bit of each bytes and stores to 'bytesToExportast' (각 바이트의 마지막 비트를 꺼내서 byteToExportLast에 저장함)
                 {
                     bytesToExportLast += bytesToExport.Substring(pointer, 2);
                     pointer += 8;
-                }//각 바이트의 마지막 비트를 꺼내서 byteToExportLast에 저장함
+                }
                 int decrease = 0, k = 0, temp = 0;
                 importedText = string.Empty;
 
@@ -129,7 +129,7 @@ namespace Steganography
                 {
                     for (int i = k; i < k + 14; i++)
                     {
-                        temp += Convert.ToInt32(bytesToExportLast.Substring(i, 1)) * (int)Math.Pow(2, (13 - decrease)); //13바꾼거임
+                        temp += Convert.ToInt32(bytesToExportLast.Substring(i, 1)) * (int)Math.Pow(2, (13 - decrease)); 
                         decrease++;
                     }
 
@@ -144,7 +144,7 @@ namespace Steganography
                     else
                     {
                         importedText += Encoding.Unicode.GetString(BitConverter.GetBytes(temp)).TrimEnd((Char)0);
-                    }//그 유니코드 값에 해당하는 문자열 꺼냄
+                    }// 그 유니코드 값에 해당하는 문자열 꺼냄 (Export the string that matched the UNICODE)
 
                     _form.ExpProgressBar.Increment(1);
                     k += 14; temp = decrease = 0;
@@ -154,10 +154,10 @@ namespace Steganography
 
 
 
-                _form.ExportTextBoxText = importedText;// 폼으로 문자열 바로 꺼냄
+                _form.ExportTextBoxText = importedText;// 폼으로 문자열 바로 꺼냄 (Extract string directly to form)
                 SetInfoLabels();
             }
-            else // 추출된 텍스트의 길이가 0이면 Stego 파일이 아니라고 판단
+            else // 추출된 텍스트의 길이가 0이면 Stego 파일이 아니라고 판단 (if the exported text length is 0, judge it's not a stego file.)
             {
                 MessageBox.Show("This is not a stego file!", CommonConstants.WarningCaption, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
