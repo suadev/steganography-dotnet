77f5df522367d95663be791bf7a7d1c4b28aef09
ur0n2 <ur0n2@sju.ac.kr>
2016-11-28 23:25:30 +0900

    Code review complete Thanks to @rlagustlr677 close #8 close #9

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Steganography.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Image1 {
+            get {
+                object obj = ResourceManager.GetObject("Image1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap picture__plus {
             get {
                 object obj = ResourceManager.GetObject("picture__plus", resourceCulture);
